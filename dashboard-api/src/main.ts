cd67c7c692076a96fa64ecb23605129ddce9b1f4
Yanik Black <yanikblake@gmail.com>
2022-03-09 16:06:52 -0500

    fix(app): added cors options

--- dashboard-api/src/main.ts
+++ dashboard-api/src/main.ts
@@ -4,8 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  const corsOptions = {
+    origin: process.env.CLIENT_ORIGIN || "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  };
+
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(3000);
