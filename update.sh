21f0bd9e74044cf128eb4148ed8d415cb7e6b68d
Yanik Black <yanikblake@gmail.com>
2022-04-06 17:40:41 -0500

    chore: added install step

--- update.sh
+++ update.sh
@@ -8,13 +8,13 @@ chmod +x ./update.sh
 cp ./Caddyfile /etc/caddy/Caddyfile
 
 echo "Rebuilding Web App..."
-cd ./dashboard-client && npm run build
+cd ./dashboard-client && npm install && npm run build
 echo "Copying files to caddy directory..."
 cp -R ./dist/* /usr/share/caddy/
 echo "Restarting caddy server"
 
 echo "Rebuilding API..."
-cd /root/app/Dispatchja-Dashboard/dashboard-api && npm run build
+cd /root/app/Dispatchja-Dashboard/dashboard-api && npm install && npm run build
 echo "Updating env file..."
 cp .env ./dist/
 echo "Restarting api..."
