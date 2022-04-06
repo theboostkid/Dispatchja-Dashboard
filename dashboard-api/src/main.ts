c00a79074d547ac1e2adc7194bed0e06bbb1366f
Yanik Black <yanikblake@gmail.com>
2022-04-06 17:24:55 -0500

    chore: added winston logger

--- dashboard-api/src/main.ts
+++ dashboard-api/src/main.ts
@@ -2,17 +2,37 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import DailyRotateFile = require('winston-daily-rotate-file');
 
 async function bootstrap() {
   const corsOptions = {
-    origin: process.env.CLIENT_ORIGIN || "*",
+    origin: process.env.CLIENT_ORIGIN || '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     credentials: true,
     allowedHeaders: 'Content-Type, Accept',
   };
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+      transports: [
+        new winston.transports.Console(),
+        new DailyRotateFile({
+          filename: 'logs/application-%DATE%.log',
+          datePattern: 'YYYY-MM-DD-HH',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '14d',
+        }),
+      ],
+    }),
+  });
   app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
