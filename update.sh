689d9f2f03740697c2aefd90a22314f7ed9a398c
Yanik Black <yanikblake@gmail.com>
2022-04-06 16:57:23 -0500

    chore: added deployment files

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Updating repository..."
+cd /root/app/Dispatchja-Dashboard/
+git checkout -- update.sh
+git pull
+chmod +x ./update.sh
+cp ./Caddyfile /etc/caddy/Caddyfile
+
+echo "Rebuilding Web App..."
+cd ./dashboard-client && npm run build
+echo "Copying files to caddy directory..."
+cp -R ./dist/* /usr/share/caddy/
+echo "Restarting caddy server"
+
+echo "Rebuilding API..."
+cd /root/app/Dispatchja-Dashboard/dashboard-api && npm run build
+echo "Updating env file..."
+cp .env ./dist/
+echo "Restarting api..."
+forever restart ./dist/main.js
+
+systemctl restart caddy
+systemctl reload caddy
